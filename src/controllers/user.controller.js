6ec836c091c70dfd37499ca867f74236e9640082
Muhammad Hussnain Shaker <mhussnainshaker251@gmail.com>
2024-08-27 19:08:22 +0500

    working on user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,33 @@
 import { asyncHandler } from '../utils/asyncHandler.js'
+import { ApiError } from '../utils/ApiError.js'
+import { User } from '../models/videotube/user.model.js'
 
 const registerUser = asyncHandler((req, res) => {
-  res.status(200).json({
-    message: 'ok and working',
-    cassage: 'User registered successfully',
+  // take user data from frontend
+  // validate the data
+  // check if user already exists
+  // check for images, avatar
+  // upload images to cloudinary
+  // create user object and enter into db
+  // check for user creation
+  // delete password and refresh token from reponse from db
+  // return response to frontend
+
+  const { username, email, fullName, password } = req.body
+
+  if (
+    [username, email, fullName, password].some((field) => field?.trim() === '')
+  ) {
+    throw new ApiError(400, 'All fields are required')
+  }
+
+  const userExist = User.findOne({
+    $or: [{ username }, { email }],
   })
+
+  if (userExist) {
+    throw new ApiError(409, 'User with username or email already exists')
+  }
 })
 
 export { registerUser }
