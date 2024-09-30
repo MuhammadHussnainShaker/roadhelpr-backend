6d5c7df59a851811519040a425e48b70559aae65
Muhammad Hussnain Shaker <mhussnainshaker251@gmail.com>
2024-09-30 19:24:33 +0500

    Fixed Login and Logout Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,183 +19,188 @@ const generateAccessAndRefreshTokens = async (userId) => {
 }
 
 const registerUser = asyncHandler(async (req, res) => {
-  // take user data from frontend
-  // validate the data
-  // check if user already exists
-  // check for images,
-  // upload images to cloudinary, check if uploaded
-  // create user object and enter into db
-  // check for user creation
-  // delete password and refresh token from reponse from db
-  // return response to frontend
+    // take user data from frontend
+    // validate the data
+    // check if user already exists
+    // check for images,
+    // upload images to cloudinary, check if uploaded
+    // create user object and enter into db
+    // check for user creation
+    // delete password and refresh token from reponse from db
+    // return response to frontend
 
-  // register user logic for roadhelpr
-  // take user data from req.body
-  // check if required fields are present, btw, all field are required
-  // then check if user already exists using email and phone number
-  // if user exists, throw error
-  // then check for profile image in req.files
-  // if profile image is present, upload it to cloudinary
-  // create user object with appropriate fields and save it to db
-  // check for user creation and retry if failed else set response varibale
-  // delete password and refresh token from response from db
-  // return response to frontend
+    // register user logic for roadhelpr
+    // take user data from req.body
+    // check if required fields are present, btw, all field are required
+    // then check if user already exists using email and phone number
+    // if user exists, throw error
+    // then check for profile image in req.files
+    // if profile image is present, upload it to cloudinary
+    // create user object with appropriate fields and save it to db
+    // check for user creation and retry if failed else set response varibale
+    // delete password and refresh token from response from db
+    // return response to frontend
 
-  // console.log('I am req',req)
-  // console.log('I am body',req.body)
-  // console.log('I am files', req.files)
+    // console.log('I am req',req)
+    // console.log('I am body',req.body)
+    // console.log('I am files', req.files)
 
-  const { fullName, phoneNumber, email, password, role } = req.body
+    console.log('Api HIT', req.body)
 
+    const { fullName, phoneNumber, email, password, role } = req.body
 
-  if (
-    [fullName, phoneNumber, email, password, role].some(
-      (field) => field?.trim() === ''
+    if (
+        [fullName, phoneNumber, email, password, role].some(
+            (field) => field?.trim() === ''
+        )
+    ) {
+        throw new ApiError(400, 'All fields are required')
+    }
+
+    const userExist = await User.findOne({
+        $or: [{ phoneNumber }, { email }],
+    })
+
+    if (userExist) {
+        throw new ApiError(
+            409,
+            'User with this phone number or email already exists'
+        )
+    }
+
+    let profileImageLocalPath
+    if (req.files?.profileImageUrl) {
+        profileImageLocalPath = req.files?.profileImageUrl[0]?.path
+    }
+
+    // changing the logic to avatar not required
+    // if (!avatarLocalPath) throw new ApiError(400, 'Avatar is required')
+
+    // trying to upload to cloudinary only when local path variables are set
+    let profileImage
+    if (profileImageLocalPath) {
+        profileImage = await uploadOnCloudinary(profileImageLocalPath)
+    }
+
+    // if (!avatar) throw new ApiError(500, 'Failed to upload avatar to cloudinary')
+
+    const user = await User.create({
+        fullName,
+        phoneNumber,
+        email,
+        password,
+        role,
+        profileImageUrl: profileImage?.url || '',
+    })
+
+    if (!user) throw new ApiError(500, 'Failed to create user in database')
+
+    const createdUser = await User.findById(user._id).select(
+        '-password -refreshToken'
     )
-  ) {
-    throw new ApiError(400, 'All fields are required')
-  }
 
-  const userExist = await User.findOne({
-    $or: [{ phoneNumber }, { email }],
-  })
-
-  if (userExist) {
-    throw new ApiError(409, 'User with this phone number or email already exists')
-  }
-
-  let profileImageLocalPath
-  if (req.files?.profileImageUrl) {
-    profileImageLocalPath = req.files?.profileImageUrl[0]?.path
-  }
-
-  // changing the logic to avatar not required
-  // if (!avatarLocalPath) throw new ApiError(400, 'Avatar is required')
-
-  // trying to upload to cloudinary only when local path variables are set
-  let profileImage
-  if (profileImageLocalPath) {
-    profileImage = await uploadOnCloudinary(profileImageLocalPath)
-  }
-
-  // if (!avatar) throw new ApiError(500, 'Failed to upload avatar to cloudinary')
-
-  const user = await User.create({
-    fullName,
-    phoneNumber,
-    email,
-    password,
-    role,
-    profileImageUrl: profileImage?.url || '',
-  })
-
-  if (!user) throw new ApiError(500, 'Failed to create user in database')
-
-  const createdUser = await User.findById(user._id).select(
-    '-password -refreshToken'
-  )
-
-  return res
-    .status(201)
-    .json(new ApiResponse(200, createdUser, 'User created successfully'))
+    return res
+        .status(201)
+        .json(new ApiResponse(200, createdUser, 'User created successfully'))
 })
 
 const loginUser = asyncHandler(async (req, res) => {
-  // take user data from req.body
-  // validate user data
-  // throw error if username or email is empty
-  // check if user exists
-  // throw error if user does not exist
-  // check if password is correct
-  // throw error if password is incorrect
-  //create refresh and access token by making separate function
-  // delete password and refresh token from user mongoose object
-  // send new user document, access token and refresh token to via secure cookies and json to frontend
+    // take user data from req.body
+    // validate user data
+    // throw error if username or email is empty
+    // check if user exists
+    // throw error if user does not exist
+    // check if password is correct
+    // throw error if password is incorrect
+    //create refresh and access token by making separate function
+    // delete password and refresh token from user mongoose object
+    // send new user document, access token and refresh token to via secure cookies and json to frontend
 
-  const { phoneNumber, email, password } = req.body
-  console.log(phoneNumber, email, password)
-  if (!phoneNumber) {
-    throw new ApiError(400, 'Phone number is required')
-  }
+    const { phoneNumber, email, password } = req.body
+    if (!phoneNumber) {
+        throw new ApiError(400, 'Phone number is required')
+    }
 
-  if (!email) {
-    throw new ApiError(400, 'Email is required')
-  }
-  
-  const user =await User.findOne({
-    $and: [{ phoneNumber }, { email }]
-  })
-  
-  if (!user) {
-    throw new ApiError(404, 'User with this phone number or email does not exist')
-  }
+    if (!email) {
+        throw new ApiError(400, 'Email is required')
+    }
 
-  const isPasswordValid = await user.isPasswordCorrect(password)
+    const user = await User.findOne({
+        $and: [{ phoneNumber }, { email }],
+    })
 
-  if (!isPasswordValid) {
-    throw new ApiError(
-      401,
-      'Incorrect password, please try again or reset your password'
+    if (!user) {
+        throw new ApiError(
+            404,
+            'User with this phone number or email does not exist'
+        )
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if (!isPasswordValid) {
+        throw new ApiError(
+            401,
+            'Incorrect password, please try again or reset your password'
+        )
+    }
+
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.refreshToken
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+        user._id
     )
-  }
 
-  const userObject = user.toObject()
+    // const loggedInUser = await User.findById(user._id)
+    // console.log(loggedInUser.refreshToken)
 
-  delete userObject.password
-  delete userObject.refreshToken
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
 
-  const {accessToken, refreshToken} =  await generateAccessAndRefreshTokens(user._id)
-
-  // const loggedInUser = await User.findById(user._id)
-  // console.log(loggedInUser.refreshToken)
-
-  const options = {
-    httpOnly: true,
-    secure: true,
-  }
-
-  res
-  .status(200)
-  .cookie('accessToken', accessToken, options)
-  .cookie('refreshToken', refreshToken, options)
-  .json(
-    new ApiResponse(
-      200,
-      {
-        user: userObject, accessToken, refreshToken
-      },
-      'User logged in successfully',
-    )
-  )
-
+    res.status(200)
+        .cookie('accessToken', accessToken, options)
+        .cookie('refreshToken', refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: userObject,
+                    accessToken,
+                    refreshToken,
+                },
+                'User logged in successfully'
+            )
+        )
 })
 
 const logoutUser = asyncHandler(async (req, res) => {
-  // console.log('logoutUser controller ran.')
-  const updatedUserDocument = await User.findByIdAndUpdate(
-      req?.user?._id,
-      {
-          $unset: {
-              refreshToken: '',
-          },
-      },
-      {
-          new: true,
-      }
-  )
+    const updatedUserDocument = await User.findByIdAndUpdate(
+        req?.user?._id,
+        {
+            $unset: {
+                refreshToken: '',
+            },
+        },
+        {
+            new: true,
+        }
+    )
 
-  // console.log(updatedUserDocument)
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
 
-  const options = {
-      httpOnly: true,
-      secure: true,
-  }
-
-  return res
-      .status(200)
-      .clearCookie('accessToken', options)
-      .clearCookie('refreshToken', options)
-      .json(new ApiResponse(200, {}, 'User logged out successfully'))
+    return res
+        .status(200)
+        .clearCookie('accessToken', options)
+        .clearCookie('refreshToken', options)
+        .json(new ApiResponse(200, {}, 'User logged out successfully'))
 })
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
--- src/models/videotube/user.model.js
+++ src/models/videotube/user.model.js
@@ -3,51 +3,51 @@ import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-      index: true,
+    {
+        username: {
+            type: String,
+            required: true,
+            // unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String, // cloudinary url
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Video',
+            },
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+            minlength: 8,
+        },
+        refreshToken: {
+            type: String,
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-      index: true,
-    },
-    fullName: {
-      type: String,
-      required: true,
-      trim: true,
-      index: true,
-    },
-    avatar: {
-      type: String, // cloudinary url
-    },
-    coverImage: {
-      type: String, // cloudinary url
-    },
-    watchHistory: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Video',
-      },
-    ],
-    password: {
-      type: String,
-      required: [true, 'Password is required'],
-      minlength: 8,
-    },
-    refreshToken: {
-      type: String,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 )
 
 // encrypt password
