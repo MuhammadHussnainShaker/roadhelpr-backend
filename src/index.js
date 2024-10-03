98861299bd38340844267e970f7aa2c0e473205e
Muhammad Hussnain Shaker <mhussnainshaker251@gmail.com>
2024-10-02 19:43:16 -0700

    reverted changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv'
 import connectDB from './db/db.js'
 import { app } from './app.js'
-import { createServer } from 'http'
-import { Server } from 'socket.io'
 
 const port = process.env.PORT || 3000
 
@@ -10,40 +8,13 @@ dotenv.config({
   path: './.env',
 })
 
-const server = createServer(app)
-
-const io = new Server(server, {
-  cors: {
-    origin: process.env.CORS_ORIGIN || '*',
-    methods: ['GET', 'POST'],
-  },
-})
-
 connectDB()
   .then(() => {
-    server.listen(port, () => {
+    app.listen(port, () => {
       console.log(`Express server listening at port no.: ${port}`)
     })
   })
   .catch((err) => console.log('MongoDB connection failed: ', err))
-
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id)
-
-  socket.on('joinRoom', ({ serviceRequestId }) => {
-    socket.join(serviceRequestId)
-    console.log(`User ${socket.id} joined room: ${serviceRequestId}`)
-  })
-
-  socket.on('sendMessage', ({ serviceRequestId, sender, message }) => {
-    const newMessage = { sender, message, time: new Date() }
-    io.to(serviceRequestId).emit('receiveMessage', newMessage)
-  })
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id)
-  })
-})
 
 const dataArray = [
   { id: 1, name: 'Item 1' },
