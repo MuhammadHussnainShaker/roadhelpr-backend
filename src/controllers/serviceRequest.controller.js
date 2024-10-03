b5a4069938dd12cb505d2998197a086d8f2cf9f5
Muhammad Hussnain Shaker <mhussnainshaker251@gmail.com>
2024-10-02 19:52:46 -0700

    created initial strucutre of service request

---
+++ src/controllers/serviceRequest.controller.js
@@ -0,0 +1,5 @@
+import { asyncHandler } from '../utils/asyncHandler.js'
+import { ApiError } from '../utils/ApiError.js'
+import { ApiResponse } from '../utils/ApiResponse.js'
+
+const createRequest = asyncHandler(async (req, res) => {})
